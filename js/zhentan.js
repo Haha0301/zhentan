36cb42a8d4ee07c288e301eb6d3635ca3b99bfcd
jianfei <wujianfei1991@gmail.com>
2017-11-26 10:04:37 +0800

    test

--- js/zhentan.js
+++ js/zhentan.js
@@ -76,4 +76,25 @@ $(function () {
     tid = setInterval(next,2000);
     next();
   })
+  // 判断移动端 
+  function browserRedirect() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+      // document.writeln("phone");
+      $('#bigOl li').click(function () {
+        $('#bigOl li').removeClass('active')
+        $(this).addClass('active')
+      })
+    }
+  }
+ 
+  browserRedirect();
 })
