afaad03a3151bcdac2ebb134bb0fc7b3a98705b2
jianfei <wujianfei1991@gmail.com>
2017-11-26 11:32:38 +0800

    test

--- js/zhentan.js
+++ js/zhentan.js
@@ -77,33 +77,33 @@ $(function () {
     next();
   })
   // 判断移动端 
-  function browserRedirect() {
-    var sUserAgent = navigator.userAgent.toLowerCase();
-    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-    var bIsAndroid = sUserAgent.match(/android/i) == "android";
-    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
-      alert(1)
-      $('#bigOl li').click(function () {
-        alert(2)
-        $('#bigOl li').removeClass('active')
-        $(this).addClass('active')
-      })
-    } else {
-      $('#bigOl li').mouseenter(function () {
-        $('#bigOl li').removeClass('active')
-        $(this).addClass('active')
-      })
-      $('#bigOl li').mouseleave(function () {
-        $('#bigOl li').removeClass('active')
-      })
-    }
-  }
+  // function browserRedirect() {
+  //   var sUserAgent = navigator.userAgent.toLowerCase();
+  //   var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+  //   var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+  //   var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+  //   var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+  //   var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+  //   var bIsAndroid = sUserAgent.match(/android/i) == "android";
+  //   var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+  //   var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+  //   if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+  //     alert(1)
+  //     $('#bigOl li').click(function () {
+  //       alert(2)
+  //       $('#bigOl li').removeClass('active')
+  //       $(this).addClass('active')
+  //     })
+  //   } else {
+  //     $('#bigOl li').mouseenter(function () {
+  //       $('#bigOl li').removeClass('active')
+  //       $(this).addClass('active')
+  //     })
+  //     $('#bigOl li').mouseleave(function () {
+  //       $('#bigOl li').removeClass('active')
+  //     })
+  //   }
+  // }
  
-  browserRedirect();
+  // browserRedirect();
 })
